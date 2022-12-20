a2a39b14bf8c75ba7542eb1a5c6f39e89dae10ad
Azaou123 <77129003+azaou123@users.noreply.github.com>
2022-12-20 18:33:06 +0100

    Add files via upload

---
+++ Number_Detection/main.js
@@ -0,0 +1,38 @@
+
+var elements = document.body.children
+var nbr = Math.floor(Math.random() * 1000)
+function validate(){
+    let inValue = elements[2]
+    function historique(result){
+        if (!result){
+            let span = document.createElement('span')
+            span.innerHTML = inValue.value+' =▶ '
+            elements[7].appendChild(span)
+        }
+        else {
+            let span = document.createElement('span')
+            span.innerHTML = inValue.value+' =✔'
+            elements[7].appendChild(span)
+            elements[1].innerHTML = elements[7].children.length +' atempts !'
+            for (let i=2;i<elements.length-1;i++){
+                elements[i].style.display = "none"
+            }
+        }
+    }
+    if (inValue.value == ''){
+        alert('Null Value is not Accepted !')
+    }
+    else if (inValue.value > nbr){
+        elements[6].innerHTML = "Greater"
+        historique(false)
+        inValue.value = ''
+    }
+    else if(inValue.value < nbr){
+        elements[6].innerHTML = "Lower"
+        historique(false)
+        inValue.value = ''
+    }
+    else {
+        historique(true)
+    }
+}
